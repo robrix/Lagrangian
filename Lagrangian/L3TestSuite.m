c587c38d50969dc7351dd8398708fc6816336a4d
Rob Rix <rob.rix@github.com>
2014-03-28 22:46:59 -0400

    Image suites’ paths are bundle paths.

--- Lagrangian/L3TestSuite.m
+++ Lagrangian/L3TestSuite.m
@@ -26,7 +26,7 @@
 }
 
 
-static inline NSString *L3PathForImageWithAddress(void(*address)(void)) {
++(NSString *)pathForImageWithAddress:(void(*)(void))address {
 	NSString *path = nil;
 	Dl_info info = {0};
 	if (dladdr((void *)address, &info)) {
@@ -35,8 +35,30 @@ static inline NSString *L3PathForImageWithAddress(void(*address)(void)) {
 	return path;
 }
 
+static void sampleAddress(void) {}
+
+l3_test(@selector(pathForImageWithAddress:)) {
+	NSString *pathForAddress = [L3TestSuite pathForImageWithAddress:sampleAddress];
+	NSBundle *bundle = [NSBundle bundleForClass:[L3TestSuite class]];
+	NSString *executablePathOfSameBundle = bundle.executablePath.stringByResolvingSymlinksInPath;
+	l3_expect(pathForAddress).to.equal(executablePathOfSameBundle);
+}
+
++(NSString *)bundlePathWithImagePath:(NSString *)imagePath {
+	imagePath = imagePath.stringByResolvingSymlinksInPath;
+	NSString *bundlePath = imagePath;
+	for (NSBundle *bundle in [[NSBundle allBundles] arrayByAddingObjectsFromArray:[NSBundle allFrameworks]]) {
+		NSString *currentBundlePath = bundle.executablePath.stringByResolvingSymlinksInPath;
+		if ([currentBundlePath isEqual:imagePath]) {
+			bundlePath = bundle.bundlePath;
+			break;
+		}
+	}
+	return bundlePath;
+}
+
 +(instancetype)suiteForImageWithAddress:(void(*)(void))address {
-	NSString *file = L3PathForImageWithAddress(address);
+	NSString *file = [self bundlePathWithImagePath:[self pathForImageWithAddress:address]];
 	L3TestSuite *suite = self.mutableRegisteredSuites[file];
 	return suite ? suite : (self.mutableRegisteredSuites[file] = [self suiteWithSourceReference:L3SourceReferenceCreate(@0, file, 0, nil, file.lastPathComponent)]);
 }
